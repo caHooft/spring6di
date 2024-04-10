688e5c0dc235cfe3afbcec94e3861f6b793e53c1
C.A. Hooft <c.a.hooft@gmail.com>
2024-04-10 14:00:23 +0200

    Added a test.
    Not validated since my UI does not show me my test output. Asked a colleague for help where I can find my test output for validation
    
    Signed-off-by: Casper Hooft <casper.hooft@ordina.nl>

---
+++ src/main/java/guru/springframework/spring6di/controllers/MyController.java
@@ -0,0 +1,15 @@
+package guru.springframework.spring6di.controllers;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello() {
+        System.out.println("Hello from MyController");
+
+        return "Hello from the return";
+    }
+
+}
